1b7ad51dccedd3554dcc517cea739161e8b3c93f
eIGato <deathmet@list.ru>
2020-05-06 01:19:32 +0300

    Move missions to nested viewset

--- hightech_cross/crosses/urls.py
+++ hightech_cross/crosses/urls.py
@@ -2,12 +2,19 @@ from django.urls import (
     include,
     path,
 )
-from rest_framework import routers
+from rest_framework_nested import routers
 
 from . import views
 
-router = routers.DefaultRouter()
-router.register(r'crosses', views.CrossViewSet)
+cross_router = routers.SimpleRouter()
+cross_router.register(r'crosses', views.CrossViewSet)
+mission_router = routers.NestedSimpleRouter(
+    cross_router,
+    r'crosses',
+    lookup='cross',
+)
+mission_router.register(r'missions', views.MissionViewSet)
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', include(cross_router.urls)),
+    path('', include(mission_router.urls)),
 ]
